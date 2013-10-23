ee4926f8ad1cd53c71c643b84b0ef6efd900ffd0
Justin <justinmew91@hotmail.com>
2013-10-22 18:03:13 -0700

    Added MethodNode, renamed CouplingVisualizer in views to CouplingVisualizerView (to avoid naming conflicts)

---
+++ src/ca/ubc/cv/structs/MethodNode.java
@@ -0,0 +1,65 @@
+package ca.ubc.cv.structs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * This node structure abstracts the IMethod object
+ * found in a call hierarchy tree. The Node class contains
+ * following fields:
+ * 
+ * className - the class that the method belongs to. 
+ * depth - the degrees of separation from the main method
+ * 		 that you are interested in. 
+ * nodesList - its list of children, which represent the list of
+ * 		IMethods that call it. 
+ * parametersList - the list of parameters the node takes. 
+ *
+ */
+public class MethodNode {
+
+	private String className;
+	private int degree;
+	private List<MethodNode> callersList; 
+	private List<Class> parametersList;
+	private Class returnType; 
+	
+	public MethodNode() {
+		callersList = new ArrayList<MethodNode>(); 
+		parametersList = new ArrayList<Class>(); 
+	}
+	
+	public int getDepth() {
+		return degree;
+	}
+	public void setDepth(int depth) {
+		this.degree = depth;
+	}
+	public String getClassName() {
+		return className;
+	}
+	public void setClassName(String className) {
+		this.className = className;
+	}
+	public List<MethodNode> getNodes() {
+		return callersList;
+	}
+	public void setNodes(List<MethodNode> nodes) {
+		this.callersList = nodes;
+	}
+	public List<Class> getParameters() {
+		return parametersList;
+	}
+	public void setParameters(List<Class> parameters) {
+		this.parametersList = parameters;
+	}
+	public Class getReturnType() {
+		return returnType; 
+	}
+	public void setReturnType(Class returnType) {
+		this.returnType = returnType;
+	}
+	
+	
+}
