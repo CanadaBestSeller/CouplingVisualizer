46f7f2dc720ec3746989a58dea3785f728a15c61
SourceDaveLive <sourcedave@live.com>
2013-11-20 13:20:43 -0800

    Experimenting with adding buttons to CV view.

---
+++ src/ca/ubc/cv/actions/IncreaseDepthAction.java
@@ -0,0 +1,15 @@
+package ca.ubc.cv.actions;
+
+import org.eclipse.jface.action.Action;
+
+import ca.ubc.cv.views.CouplingVisualizerView;
+
+public class IncreaseDepthAction extends Action {
+
+	private CouplingVisualizerView view;
+	
+	private IncreaseDepthAction(CouplingVisualizerView pView) {
+		
+	}
+
+}
